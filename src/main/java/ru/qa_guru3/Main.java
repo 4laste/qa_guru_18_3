deb96401b201c7530d46411156afdd41a5af6dc9
Pavel Nasonov <4laste@gmail.com>
2024-03-17 18:58:22 +0100

    Changed Hello

--- src/main/java/ru/qa_guru3/Main.java
+++ src/main/java/ru/qa_guru3/Main.java
@@ -2,6 +2,6 @@ package ru.qa_guru3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello son of the bitch");
     }
 }
